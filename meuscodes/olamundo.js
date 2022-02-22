941ee43bf9412253877359e4ec4dea1940091512
Deborah Nazareth <cristina.nazareth@outlook.com>
2022-02-21 22:42:41 -0300

    Reṕositório PAMII

---
+++ meuscodes/olamundo.js
@@ -0,0 +1,8 @@
+console.log('Olá mundo')
+var a = 2;
+console.log('Esse é o conteúdo de a = ',a)
+var b = 3;
+console.log('Esse é o resultado de ', a , '+', b , '=', a+b)
+let nome = 'Debs'
+console.log('Meu nome é', nome)
+console.log(nome * a)
